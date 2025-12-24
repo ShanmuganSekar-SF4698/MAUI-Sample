ee3d3b6032f1b70b95030dc0ed6f7be70466f72e
ShanmuganSekar-SF4698 <shanmugan.sekar@syncfusion.com>
2025-12-24 09:50:59 +0530

    Added sample

---
+++ MauiApp1/MauiApp1/GraphicsDrawable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace MauiApp1
+{
+    internal class GraphicsDrawable : IDrawable
+    {
+
+        public GraphicsDrawable() 
+        {
+        }
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            Debug.WriteLine($"Height: {dirtyRect.Height}, Width: {dirtyRect.Width}");
+        }
+    }
+}
---
+++ MauiApp1/MauiApp1/ParentView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiApp1
+{
+    internal class ParentView : ContentView
+    {
+        public ParentView() 
+        {
+            this.WidthRequest = 100;
+            this.HeightRequest = 50;
+
+            this.Content = new CustomGraphicsView();    
+        }
+    }
+
+    internal class CustomGraphicsView : GraphicsView
+    {
+        public CustomGraphicsView() 
+        {
+            Drawable = new GraphicsDrawable();
+        }
+    }
+}
